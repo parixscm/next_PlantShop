8a05f6fc450908622623e3f0ecfd89ccfa1d962c
parixscm <parixscm@gmail.com>
2023-02-15 17:00:03 +0900

    Feat: init project files

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main>
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
+}
