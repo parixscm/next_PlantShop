9c33c70029c97f8423c29361e3d11e3de476a2e9
parixscm <parixscm@gmail.com>
2023-02-18 00:04:29 +0900

    Refactor: ADD common Page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,18 @@
-import Head from "next/head";
-import Title from "../components/Title";
+import Page from "../components/Page";
+import ProductCard from "../components/ProductCard";
 import { getProducts } from "lib/products";
-import ProductCard from "@/components/ProductCard";
 
 export default function HomePage({ products }) {
   return (
-    <>
-      <Head>
-        <title>Next Shop</title>
-      </Head>
-      <main className="px-6 py-4">
-        <Title>Next Shop</Title>
-        <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-          {products.map(product => (
-            <li key={product.id}>
-              <ProductCard product={product} />
-            </li>
-          ))}
-        </ul>
-      </main>
-    </>
+    <Page title="Indoor Plants">
+      <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+        {products.map(product => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </ul>
+    </Page>
   );
 }
 
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,33 +1,26 @@
-import Head from "next/head";
 import Image from "next/image";
-import Title from "../../components/Title";
+import Page from "../../components/Page";
 import { ApiError } from "lib/api";
 import { getProduct, getProducts } from "lib/products";
 
 export default function ProductPage({ product }) {
   return (
-    <>
-      <Head>
-        <title>Next Shop</title>
-      </Head>
-      <main className="px-6 py-4">
-        <Title>{product.title}</Title>
-        <div className="flex flex-col lg:flex-row">
-          <div>
-            <Image
-              src={product.imageUrl}
-              alt="product image"
-              width={640}
-              height={480}
-            />
-          </div>
-          <div className="flex-1 mt-4 space-y-2 lg:ml-4 lg:mt-0">
-            <p className="text-sm">{product.description}</p>
-            <p className="text-lg font-bold">{product.price}</p>
-          </div>
+    <Page title={product.title}>
+      <div className="flex flex-col lg:flex-row">
+        <div>
+          <Image
+            src={product.imageUrl}
+            alt="product image"
+            width={640}
+            height={480}
+          />
         </div>
-      </main>
-    </>
+        <div className="flex-1 mt-4 space-y-2 lg:ml-4 lg:mt-0">
+          <p className="text-sm">{product.description}</p>
+          <p className="text-lg font-bold">{product.price}</p>
+        </div>
+      </div>
+    </Page>
   );
 }
 
