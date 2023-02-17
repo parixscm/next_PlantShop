158b91cdbce032f8b9a5a3ed36d8b16b41415460
parixscm <parixscm@gmail.com>
2023-02-17 15:26:02 +0900

    Feat: ADD ProductCard component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Title from "../components/Title";
 import { getProducts } from "lib/products";
+import ProductCard from "@/components/ProductCard";
 
 export default function HomePage({ products }) {
   return (
@@ -11,10 +12,10 @@ export default function HomePage({ products }) {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
           {products.map(product => (
             <li key={product.id}>
-              <Link href={`/products/${product.id}`}>{product.title}</Link>
+              <ProductCard product={product} />
             </li>
           ))}
         </ul>
