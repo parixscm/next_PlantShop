5d2cb016bd81175bf909a1048c3cddb2b088f12a
parixscm <parixscm@gmail.com>
2023-02-18 16:54:44 +0900

    Feat: ADD states and validation to form in sign-in page

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,3 +1,16 @@
-export default function Input(type) {
-  return <input type={type} className="w-80 px-3 py-1 rounded-sm border" />;
+export default function Input({ type, required, value, onChange }) {
+  return (
+    <input
+      type={type}
+      required={required}
+      value={value}
+      onChange={onChange}
+      className="w-80 px-3 py-1 rounded-sm border"
+    />
+  );
 }
+
+// 💡 위 코드와 동일
+// export default function Input(props) {
+//   return <input {...props} className="w-80 px-3 py-1 rounded-sm border" />;
+// }
--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -2,16 +2,39 @@ import Page from "../components/Page";
 import Field from "../components/Field";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useState } from "react";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <Page title="Sign In">
-      <form className="flex flex-col space-y-2 items-start">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col space-y-2 items-start"
+      >
         <Field label="Email">
-          <Input type="email" />
+          <Input
+            type="email"
+            required
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input
+            type="password"
+            required
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
         </Field>
         <Button type="submit">Sign In</Button>
       </form>
