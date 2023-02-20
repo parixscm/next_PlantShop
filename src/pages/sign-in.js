e02291dd6bdf3a67faa79b967299629598760bf1
parixscm <parixscm@gmail.com>
2023-02-20 15:22:02 +0900

    Refactor: ADD useSignIn hook

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -3,36 +3,21 @@ import Field from "../components/Field";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { useState } from "react";
-import { fetchJson } from "../../lib/api";
 import { useRouter } from "next/router";
-import { useMutation, useQueryClient } from "react-query";
+import { useSignIn } from "hooks/user";
 
 export default function SignIn() {
   const router = useRouter();
-  const queryClient = new useQueryClient();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const mutation = useMutation(() =>
-    fetchJson("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    })
-  );
+  const { signIn, signInError, signInLoading } = useSignIn();
 
   const handleSubmit = async event => {
     event.preventDefault();
     setEmail("");
     setPassword("");
-    try {
-      const user = await mutation.mutateAsync();
-      // 💡 data is updated immediately
-      queryClient.setQueryData("user", user);
-      router.push("/");
-    } catch (err) {
-      //
-    }
+    const isValid = await signIn(email, password);
+    if (isValid) router.push("/");
   };
 
   return (
@@ -57,10 +42,8 @@ export default function SignIn() {
             onChange={event => setPassword(event.target.value)}
           />
         </Field>
-        {mutation.isError && (
-          <p className="text-red-500">Invalid credentials</p>
-        )}
-        {mutation.isLoading ? (
+        {signInError && <p className="text-red-500">Invalid credentials</p>}
+        {signInLoading ? (
           <p>Loading...</p>
         ) : (
           <Button type="submit">Sign In</Button>
